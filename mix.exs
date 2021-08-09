8ed5c95372b62f87475409e941740d13736b6486
Sebastian Borrazas <seba.borrazas@gmail.com>
2021-08-09 18:38:16 -0300

    Add mix docs specification

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule ExBitset.MixProject do
 
       description: "Bitsets implementation",
       package: package(),
+      docs: docs(),
 
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -25,7 +26,8 @@ defmodule ExBitset.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:benchfella, "~> 0.3.0", only: :dev}
+      {:benchfella, "~> 0.3.0", only: :dev},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 
@@ -48,6 +50,14 @@ defmodule ExBitset.MixProject do
       maintainers: ["Sebastian Borrazas"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/sborrazas/ex_bitset"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "ExBitset",
+      source_url: "https://github.com/sborrazas/ex_bitset",
+      extras: ["README.md"]
     ]
   end
 end
